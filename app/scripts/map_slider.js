3e41a90c0e8d252fee0900d20afe53ea49cb0c7f
anabranch <wac.chambers@gmail.com>
2015-05-11 12:30:18 -0700

    better code organization

--- app/scripts/map_slider.js
+++ app/scripts/map_slider.js
@@ -3,65 +3,44 @@ d3.csv("data/map_slider.csv", function(data) {
 });
 
 function drawMarkerArea(csvfile) {
-    var groupname = "marker-area";
+    var groupname = "crimes";
+    var data = crossfilter(csvfile);
 
-    var mapChart = dc.leafletMarkerChart("#dcjs-map", groupname),
-        categoryChart = dc.rowChart("#category", groupname),
-        gameChart = dc.rowChart("#game", groupname),
-        hourChart = dc.barChart("#crimesbyhour", groupname),
-        winChart = dc.barChart("#win", groupname),
-        homeChart = dc.barChart("#home", groupname),
-        countInfo = dc.dataCount("#dc-data-count", groupname);
 
-    var data = crossfilter(csvfile),
-        all = data.groupAll(),
-        crime = data.dimension(function(d) {
-            return d.lon + "," + d.lat;
-        }),
-        crimeGroup = crime.group().reduceCount(),
-        category = data.dimension(function(d) {
-            return d.category;
-        }),
-        categoryGroup = category.group().reduceCount(),
-        game = data.dimension(function(d) {
-            return d.game;
-        }),
-        gameGroup = game.group().reduceCount(),
-        crimesbyhour = data.dimension(function(d) {
-            return d.hour;
-        }),
-        crimesbyhourGroup = crimesbyhour.group().reduceCount(),
-        win = data.dimension(function(d) {
-            return d.win;
-        }),
-        winGroup = win.group().reduceCount(),
-        home = data.dimension(function(d) {
-            return d.home;
-        }),
-        homeGroup = home.group().reduceCount();
-
-    countInfo
-        .dimension(data)
-        .group(all);
-
+    var mapChart = dc.leafletMarkerChart("#dcjs-map", groupname);
+    var crime = data.dimension(function(d) {
+        return d.lon + "," + d.lat;
+    });
+    var crimeGroup = crime.group().reduceCount();
     mapChart
         .dimension(crime)
         .group(crimeGroup)
         .width(600)
         .height(600)
         .zoom(14)
-        // .filterByArea(true);
         .center([37.7595, -122.427]);
 
+
+    var categoryChart = dc.rowChart("#category", groupname);
+    var category = data.dimension(function(d) {
+        return d.category;
+    });
+    var categoryGroup = category.group().reduceCount();
     categoryChart
         .dimension(category)
         .group(categoryGroup)
-        .height(500)
+        .height(600)
         .width(270)
         .elasticX(true)
         .colors(["#2ca25f"])
         .xAxis().ticks(2).tickFormat(d3.format("s"));
 
+
+    var gameChart = dc.rowChart("#game", groupname);
+    var game = data.dimension(function(d) {
+        return d.game;
+    });
+    var gameGroup = game.group().reduceCount();
     gameChart
         .dimension(game)
         .group(gameGroup)
@@ -71,6 +50,12 @@ function drawMarkerArea(csvfile) {
         .colors(["#2b8cbe"])
         .xAxis().ticks(2).tickFormat(d3.format("s"));
 
+
+    var hourChart = dc.barChart("#crimesbyhour", groupname);
+    var crimesbyhour = data.dimension(function(d) {
+        return d.hour;
+    });
+    var crimesbyhourGroup = crimesbyhour.group().reduceCount();
     hourChart
         .dimension(crimesbyhour)
         .group(crimesbyhourGroup)
@@ -80,23 +65,33 @@ function drawMarkerArea(csvfile) {
         .x(d3.scale.linear().domain([0, 23]))
         .yAxis().ticks(2).tickFormat(d3.format("s"));
 
-    winChart
-        .dimension(win)
-        .group(winGroup)
-        .xUnits(dc.units.ordinal)
-        .width(200)
-        .elasticY(true)
-        .colors(["#2ca25f"])
-        .yAxis().ticks(2).tickFormat(d3.format("s"));
 
-    homeChart
-        .dimension(home)
-        .group(homeGroup)
-        .xUnits(dc.units.ordinal)
-        .width(200)
-        .elasticY(true)
-        .colors(["#2ca25f"])
-        .yAxis().ticks(2).tickFormat(d3.format("s"));
+    // var winChart = dc.barChart("#win", groupname);
+    // var win = data.dimension(function(d) {
+    //     return d.win;
+    // });
+    // var winGroup = win.group().reduceCount();
+    // winChart
+    //     .dimension(win)
+    //     .group(winGroup)
+    //     .xUnits(dc.units.ordinal)
+    //     .width(200)
+    //     .elasticY(true)
+    //     .colors(["#2ca25f"])
+    //     .yAxis().ticks(2).tickFormat(d3.format("s"));
+    // var homeChart = dc.barChart("#home", groupname);
+    // var home = data.dimension(function(d) {
+    //     return d.home;
+    // });
+    // var homeGroup = home.group().reduceCount();
+    // homeChart
+    //     .dimension(home)
+    //     .group(homeGroup)
+    //     .xUnits(dc.units.ordinal)
+    //     .width(200)
+    //     .elasticY(true)
+    //     .colors(["#2ca25f"])
+    //     .yAxis().ticks(2).tickFormat(d3.format("s"));
 
     dc.renderAll(groupname);
 }
