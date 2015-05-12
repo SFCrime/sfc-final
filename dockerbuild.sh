3a5c21a79d1914d100748fc3b9a7ebca05fd1f01
anabranch <wac.chambers@gmail.com>
2015-05-12 13:29:29 -0700

    Removed fav icon, better title

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,6 +1,3 @@
 grunt build
-cd dist/images/
-for file in *1.png ; do mv $file ${file:9} ; done
-cd ../..
-docker build -t anabranch/sfcrime-viewer .
+:docker build -t anabranch/sfcrime-viewer .
 docker push anabranch/sfcrime-viewer
